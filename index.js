d88e168c94cfc17718720649f3f838d77a7cb5f7
dejiobasan <dejiobasan@yahoo.com>
2024-06-03 19:31:01 +0100

    Work

---
+++ index.js
@@ -0,0 +1,19 @@
+const puppeteer = require("puppeteer");
+require("dotenv").config();
+
+async () => {
+  const browser = await puppeteer.launch({ headless: false });
+
+  const page = await browser.newPage();
+
+  await page.goto(process.env.URL);
+
+  await page.waitForSelector("h1");
+
+  await page.waitForSelector("form");
+
+  await page.type("#username", process.env.USERNAME);
+  await page.type("#password", process.env.PASSWORD);
+
+  await page.click("submitButton"); //click login button
+};
