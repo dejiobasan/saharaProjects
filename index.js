4558b2057d1da647a70658c6e68c508444f0fb65
dejiobasan <dejiobasan@yahoo.com>
2024-06-05 11:17:23 +0100

    work

--- index.js
+++ index.js
@@ -3,23 +3,43 @@ require("dotenv").config();
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: false,
+    args: [
+      "--start-maximized",
+      "--use-fake-ui-for-media-stream",
+      "--use-fake-device-for-media-stream",
+      "--disable-web-security",
+    ],
   });
+  const context = browser.defaultBrowserContext();
+  await context.overridePermissions(process.env.URL, ["geolocation"]);
 
   const page = await browser.newPage();
+  const width = 1080;
+  const height = 1024;
+
+  await page.setViewport({
+    width: Math.ceil(width),
+    height: Math.ceil(height),
+  }); //sets the viewport of the webpage
 
   await page.goto(process.env.URL);
 
   await page.waitForSelector("h1");
 
-  await page.waitForSelector("form");
+  await page.waitForSelector(".form");
 
-  //   await page.type("email", process.env.USERNAME);
-  //   await page.type("password", process.env.PASSWORD);
-  await page.locator("input").fill(process.env.USERNAME);
-  await page.locator('input').fill(process.env.PASSWORD);
+  const inputs = await page.$$(".inputClass"); // gets the both input selectors by their class names
+  await inputs[0].type(process.env.USERNAME); //login the username
+  await inputs[1].type(process.env.PASSWORD); // login the password
 
-  await page.screenshot({ path: "IEPAGE.png" });
+  //   await page.screenshot({ path: "IEPAGE.png" });
 
-  //   await page.click("login"); //click login button
+  await page.click("#login"); //click login button
+
+  await page.waitForSelector("h1")
+
+  await page.locator('button').click();
+
+//   const buttons = await page.$$("button");
 })();
