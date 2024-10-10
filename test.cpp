2311b9636f7b03ac5ecfe1a5e52f134e0857f2cc
dejiobasan <dejiobasan@yahoo.com>
2024-10-10 10:50:50 +0100

    .

--- test.cpp
+++ test.cpp
@@ -3,6 +3,13 @@ using namespace std;
 
 
 int main() {
-    cout << "Hello World!";
+    int length = 5;
+    int width = 4;
+
+    int area = length * width;
+
+    cout <<"Length is : "<< length << "\n";
+    cout <<"Width is : "<< width << "\n";
+    cout <<"Area is : "<< area << "\n";
     return 0;
 }
